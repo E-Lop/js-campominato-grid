565f7d1b4f77798c289fef1e7a5822c4dc78b7b4
E-Lop <edmondolopez@gmail.com>
2022-06-14 14:07:42 +0200

    analisi della consegna

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ In seguito attraverso dei prompt l'utente inserisce un numero alla volta finche 
 se il numero è presente nella lista dei numeri generati, abbiamo calpestato una bomba, il gioco finisce con un messaggio di errore
 Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili. In questo caso il gioco finisce con un messaggio di vittoria.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba. */
+
+//  ANALISI
+// FASE PREPARATORIA
+// chiedo livello difficoltà a utente (tramite prompt)
+// se 1: 1-100; se 2: 1-81; se 3: 1-49
+// genero 16 bombe, numeri compresi tra 1 e maxRange (100, 81, 49)
+// maxTentativi = maxRange - bombe (16)
+
+// FASE LOGICA
+// numeroUtente (via input): se === bomba ---> gioco finito e alert sconfitta + punteggio
+// altrimenti:
+//      se non già incluso, inserisco in array numeri azzeccati
+//          se maxTentativi raggiunto ---> gioco finito e alert vittoria
+//          altrimenti gioco continua
+
+// chiedo all'utente il livello di difficoltà del gioco
+const difficultyLevel = prompt(
+  'Dimmi il livello di difficoltà a cui vuoi giocare: 1, 2 o 3'
+);
